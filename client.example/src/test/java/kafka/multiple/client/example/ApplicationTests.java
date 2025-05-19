d2fc2439479bada9b1191252d3b161c3ef9421c2
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-05-20 03:00:03 +0530

    kafka multiple

---
+++ client.example/src/test/java/kafka/multiple/client/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka.multiple.client.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
