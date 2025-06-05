797dab19176cefe294d086684ac650722c7e9936
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-06-05 18:00:23 +0530

    commit

--- webfluxexample/docker-build-run.bat
+++ webfluxexample/docker-build-run.bat
@@ -8,5 +8,9 @@ call mvn clean install -DskipTests
 echo Building Docker image...
 docker build -t webfluxexample .
 
+# If you uncomment below 2 lines then it will push you just created docker images(webfluxexample) to DockerHub.
+#docker tag webfluxexample shahabuddin999/webfluxexample:latest 
+#docker push shahabuddin999/webfluxexample:latest
+
 echo Running Docker container...
 docker run -p 8080:8080 webfluxexample
