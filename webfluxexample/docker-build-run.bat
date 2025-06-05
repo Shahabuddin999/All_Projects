0ca5f27e0f6986b92ce93eb3898bf0afa61ae22c
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-06-05 17:32:02 +0530

    commit

---
+++ webfluxexample/docker-build-run.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Building Spring Boot project with Maven...
+
+#cd /d D:\OLX-Apps\OLX-Updated.zip\webfluxexample
+
+call mvn clean install -DskipTests
+
+echo Building Docker image...
+docker build -t webfluxexample .
+
+echo Running Docker container...
+docker run -p 8080:8080 webfluxexample
