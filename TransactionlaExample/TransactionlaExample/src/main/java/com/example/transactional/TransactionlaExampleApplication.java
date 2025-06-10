94876f1986901c0c28a56a5c450ca5a0be8c55c5
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-06-10 22:37:26 +0530

    commit

---
+++ TransactionlaExample/TransactionlaExample/src/main/java/com/example/transactional/TransactionlaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionlaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionlaExampleApplication.class, args);
+	}
+
+}
