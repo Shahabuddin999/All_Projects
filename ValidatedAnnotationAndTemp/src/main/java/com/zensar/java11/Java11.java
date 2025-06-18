bc2d95ac3ece59296b400701c2006a81dd0affaf
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-06-19 02:31:40 +0530

    commit

---
+++ ValidatedAnnotationAndTemp/src/main/java/com/zensar/java11/Java11.java
@@ -0,0 +1,64 @@
+package com.zensar.java11;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import javax.validation.constraints.NotNull;
+public class Java11 {
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+		List<String> list = List.of("Shahabuddin","Ansari","Koraon");
+		list.stream().forEach((var name)->System.out.println(name));
+		list.stream().forEach((@NotNull var name)->System.out.println(name));
+		list.stream().forEach((final var name)->System.out.println(name));
+		
+		List<String> copy = List.copyOf(list);
+		System.out.println("Copied list : "+copy);
+		
+		System.out.println("  hello  ".isBlank());         // false
+		System.out.println("java\nrocks".lines().count());         // 3 // Stream of "java", "rocks"
+		System.out.println("foo".repeat(3));               // "foofoofoo"
+		System.out.println("  trim  ".strip());            // Better Unicode-aware trim
+		System.out.println("  trim  ".trim()); 
+		System.out.println("  left ".stripLeading());      // "left "
+		System.out.println(" right ".stripTrailing());     // " right"
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+		    .uri(URI.create("https://api.github.com"))
+		    .build();
+
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
+		
+		CompletableFuture<HttpResponse<String>> asyncResponse = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(asyncResponse.get().body());
+		
+		Optional<String> name = Optional.empty();
+		if (name.isEmpty()) {
+		    System.out.println("No name");
+		}
+		String filePath = "D:\\OLX-Apps\\OLX-Updated.zip\\ValidatedAnnotationAndTemp\\src\\main\\java\\com\\zensar\\java11\\file.txt";
+		String content = Files.readString(Path.of(filePath));
+		System.out.println(content);
+		Files.writeString(Path.of(filePath), "Hello Java 11");
+		// You can run .java files directly without compiling, for example : java Hello.java
+		// Java 11 removed some older features: like (Applets, Java EE modules (like javax.xml.bind), JavaFX (now separate from JDK))
+
+			
+
+			
+
+			
+
+	}
+
+}
