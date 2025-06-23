28cfdd02d865cc43560f49bd8d41c2d34a25745d
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-06-23 18:01:51 +0530

    commit

---
+++ WebFluxMono/src/main/java/com/webflux/example/MonowebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonowebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonowebfluxApplication.class, args);
+	}
+
+}
