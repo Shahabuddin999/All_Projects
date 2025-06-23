6458917ef4bf88ee22af8afa174a4d68afd5000a
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-06-23 12:18:32 +0530

    given comment for aws

--- webfluxexample/src/main/java/com/webflux/demo/controller/S3Controller.java
+++ webfluxexample/src/main/java/com/webflux/demo/controller/S3Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.webflux.demo.service.S3Service;
 /*
  * This endpoint will work when you will deploy it on EC2 instance because it can not access DefaultCredentialsProvider from AWS  
- *
+ * I have created awsCommand.txt file in resource folder where I have written some command go through and follow
  */
 
 
