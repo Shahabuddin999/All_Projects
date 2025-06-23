f72b578c73380b7392a65dbd285eb06abea54020
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-06-23 21:45:48 +0530

    ccc

--- WebFluxMono/src/main/java/com/webflux/example/MonowebfluxApplication.java
+++ WebFluxMono/src/main/java/com/webflux/example/MonowebfluxApplication.java
@@ -8,7 +8,7 @@ public class MonowebfluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonowebfluxApplication.class, args);
-		System.out.println("New pushed gone to Git hub..........New chages done");
+		System.out.println("New pushed gone to Git hub.........<<<<<<<<<>>>>>>>>>>>.New chages pushed");
 	}
 
 }
