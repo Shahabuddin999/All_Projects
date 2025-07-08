7d0bd941e79a4a8eb0796919a16387c0deb3a520
Shahabuddin999 <ansari.shahabuddin999@gmail.com>
2025-07-08 18:16:07 +0530

    commit

--- OLX-Login/src/main/java/com/olx/security/UserDetailsServiceImpl.java
+++ OLX-Login/src/main/java/com/olx/security/UserDetailsServiceImpl.java
@@ -25,25 +25,7 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
 	PasswordEncoder passwordEncoder; 
-	
-//	@Override
-//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		List<UserEntity> userEntities = userRepo.findByUsername(username);
-//		if(userEntities.isEmpty()) {
-//			throw new UsernameNotFoundException(username);
-//		}
-//		UserEntity userEntity = userEntities.get(0);
-//		Collection<GrantedAuthority> authority = new ArrayList<>();
-//		String[] roles = userEntity.getRoles().split(",");
-//		for(int i=0; i<roles.length; i++)
-//			authority.add(new SimpleGrantedAuthority(roles[i]));
-//		UserDetails userDetails = new User(username, passwordEncoder.encode(userEntity.getPassword()), authority);
-//		boolean check = passwordEncoder.matches(userEntity.getPassword(), passwordEncoder.encode(userEntity.getPassword()));
-//		if(check) 
-//			System.out.println("successfully validated...");
-//		return userDetails;
-//	}
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
