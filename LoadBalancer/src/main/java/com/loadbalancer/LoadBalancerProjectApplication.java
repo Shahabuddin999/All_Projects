6410ac27a657258c97ec5195c7469b962bf55984
Shahabuddin <Shahabuddin@host.docker.internal>
2026-01-21 03:53:55 +0530

    commit

---
+++ LoadBalancer/src/main/java/com/loadbalancer/LoadBalancerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.loadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancerProjectApplication.class, args);
+	}
+
+}
